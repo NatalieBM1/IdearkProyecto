c544ea0804c3e903d7f5678f70642f4bdef12cc8
NatalieBM1 <127702162+NatalieBM1@users.noreply.github.com>
2025-11-06 13:31:54 +0000

    Cambios10

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
@@ -27,9 +30,13 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
   },
   preview: {
-    host: '0.0.0.0', 
-    port: process.env.PORT || 8080, 
-    allowedHosts: ['ideark-proyecto.onrender.com'], 
-    open: false, 
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080,
+    allowedHosts: ['ideark-proyecto.onrender.com'],
+    open: false,
+  },
+  define: {
+    
+    'process.env': process.env,
   },
 });
